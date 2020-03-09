e3139094097ee2628ab0b5009fa9cec6758e4547
ShwetaDubey <ShwetaDubey@192.168.2.5>
2020-03-09 09:38:28 +0530

    thanks

---
+++ final-framework-testng/src/com/training/pom/LoginRetailPOM2.java
@@ -0,0 +1,38 @@
+package com.training.pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginRetailPOM2 {
+	private WebDriver driver; 
+	
+	public LoginRetailPOM2(WebDriver driver) {
+		this.driver = driver; 
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(id="input-email")
+	private WebElement userName; 
+	
+	@FindBy(id="input-password")
+	private WebElement password;
+	
+	@FindBy(xpath="//*[@value='Login']") 
+	private WebElement loginBtn; 
+	
+	public void sendUserName(String userName) {
+		this.userName.clear();
+		this.userName.sendKeys(userName);
+	}
+	
+	public void sendPassword(String password) {
+		this.password.clear(); 
+		this.password.sendKeys(password); 
+	}
+	
+	public void clickLoginBtn() {
+		this.loginBtn.click(); 
+	}
+}
